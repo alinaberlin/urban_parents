c7ea8931b5e85fe64e57fce60880f6998a8af721
masapasa <apyakurel@gmail.com>
2019-04-25 14:38:39 +0200

    map popup

--- routes/details.js
+++ routes/details.js
@@ -9,118 +9,146 @@ const { getReverseGeoCoding } = require("../lib/services");
 
 // parent route
 router.get("/registration", ensureLogin.ensureLoggedIn(), (req, res, next) => {
-    const currentParent = req.user;
-    res.render("parent", {
-        parent: currentParent
-    });
+  const currentParent = req.user;
+
+  res.render("parent", {
+    parent: currentParent
+  });
 });
 
 router.get("/parent", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
-    const currentParent = req.user;
-    res.render("parent_details", { parent: currentParent });
+  const currentParent = req.user;
+  res.render("parent_details", { parent: currentParent });
 });
 router.post("/parent", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
-    const currentParent = req.user;
-    const {
-        username,
-        firstName,
-        lastName,
-        age,
-        email,
-        language,
-        nationality,
-        gender,
-        address,
-        postcode,
-        city,
-        country,
-        ocupation,
-        pictureUrl
-    } = req.body;
-    const location = {
-        type: "Point",
-        Address: address,
-        Postcode: postcode,
-        City: city,
-        Country: country
-    };
-    const response = await getReverseGeoCoding(address, postcode, city, next);
-    const data = response.data;
-    location.Coordinates = data.features[0].geometry.coordinates;
-    const newParent = Object.assign(currentParent, {
-        username,
-        firstName,
-        lastName,
-        email,
-        age,
-        language,
-        nationality,
-        gender,
-        location,
-        ocupation,
-        pictureUrl
-    });
-    newParent
-        .save()
-        .then(parent => {
-            Parent.find({ language: language })
-                .then(matches => {
-                    res.render("parent_details", { parent, matches });
-                })
-                .catch(error => {
-                    console.log(error);
-                    res.render("parent_details", { parent });
-                });
-        })
-        .catch(error => {
-            console.log(error);
-        });
+  const currentParent = req.user;
+
+  const {
+    username,
+    firstName,
+    lastName,
+    age,
+    email,
+    language,
+    nationality,
+    gender,
+    address,
+    postcode,
+    city,
+    country,
+    ocupation,
+    pictureUrl
+  } = req.body;
+  const location = {
+    type: "Point",
+    Address: address,
+    Postcode: postcode,
+    City: city,
+    Country: country
+  };
+  const response = await getReverseGeoCoding(address, postcode, city, next);
+  const data = response.data;
+  location.Coordinates = data.features[0].geometry.coordinates;
+  console.log(location.coordinates);
+
+  Parent.findOneAndUpdate(
+    { _id: currentParent._id },
+    {
+      username,
+      firstName,
+      lastName,
+      age,
+      email,
+      language,
+      nationality,
+      gender,
+      address,
+      postcode,
+      city,
+      country,
+      ocupation,
+      pictureUrl,
+      location
+    }
+  ).then(() => {
+    console.log("edited");
+    res.redirect("/");
+  });
+
+  //   const newParent = Object.assign(currentParent, {
+  //     username,
+  //     firstName,
+  //     lastName,
+  //     email,
+  //     age,
+  //     language,
+  //     nationality,
+  //     gender,
+  //     ocupation,
+  //     pictureUrl
+  //   });
+
+  //   newParent
+  //     .save()
+  //     .then(parent => {
+  //       Parent.find({ language: language })
+  //         .then(matches => {
+  //           res.render("parent_details", { parent, matches });
+  //         })
+  //         .catch(error => {
+  //           console.log(error);
+  //           res.render("parent_details", { parent });
+  //         });
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
 });
 
 //child route
 router.get("/parent/:parentId/child", (req, res, next) => {
-    res.render("child");
+  res.render("child");
 });
 
 router.post("/child/add", (req, res, next) => {
-    const { name, languages, age, gender } = req.body;
-    const newChild = new Child({
-        name,
-        languages,
-        age,
-        gender
-    });
-    newChild.save().then(child => {
-        Child.find({ age: age })
-            .then(matches_child => {
-                res.render("child_details", { child, matches_child });
-            })
-            .catch(error => {
-                console.log(error);
+  const { name, languages, age, gender } = req.body;
+  const newChild = new Child({
+    name,
+    languages,
+    age,
+    gender
+  });
+  newChild.save().then(child => {
+    Child.find({ age: age })
+      .then(matches_child => {
+        res.render("child_details", { child, matches_child });
+      })
+      .catch(error => {
+        console.log(error);
 
-                res.render("child_details", { child });
-            });
-    });
+        res.render("child_details", { child });
+      });
+  });
 });
 //activity
 router.get("/parent/:parentId/activity", (req, res, next) => {
-    res.render("activity");
+  res.render("activity");
 });
 router.post("/activity/add", (req, res, next) => {
-    const { location, activityType, time } = req.body;
-    const newActivity = new Activity({
-        location,
-        activityType,
-        time
+  const { location, activityType, time } = req.body;
+  const newActivity = new Activity({
+    location,
+    activityType,
+    time
+  });
+  newActivity
+    .save()
+    .then(activity => {
+      res.render("activity_details", activity);
+    })
+    .catch(error => {
+      console.log(error);
     });
-    newActivity
-        .save()
-        .then(activity => {
-            res.render("activity_details", activity);
-        })
-        .catch(error => {
-            console.log(error);
-        });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Place = require('../models/Place.js')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/add-places', (req, res, next) => {
-  res.render('add-places')
-})
+router.get("/add-places", (req, res, next) => {
+  res.render("add-places");
+});
 
-router.post('/add-places', (req, res, next) => {
+router.post("/add-places", (req, res, next) => {
   const newPlace = new Place({
     name: req.body.name,
     place: req.body.place,
     location: {
       coordinates: [req.body.longitude, req.body.latitude]
     }
-  })
+  });
 
-  newPlace.save()
+  newPlace
+    .save()
     .then(() => {
-      res.redirect('/')
+      res.redirect("/");
     })
     .catch(err => {
-      console.log(err)
-    })
-})
+      console.log(err);
+    });
+});
 
 module.exports = router;
